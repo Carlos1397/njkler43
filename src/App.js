1f54843fd1724c01dc44523fff770242f1874a48
Carlos Viera <pisis_13_3@hotmail.com>
2022-02-06 15:29:17 -0500

    add filter

--- src/App.js
+++ src/App.js
@@ -5,19 +5,36 @@ import posts from './posts'
 // Modifica el componente App para implmentar la funcionalidad requerida
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      filtro: "",
+    }
+    this.actualizarFiltro = this.actualizarFiltro.bind(this);
+  }
+  actualizarFiltro(e) {
+    this.setState({
+      filtro: e.target.value
+    })
+  }
   render() {
+    const { filtro } = this.state;
     return (
       <div>
         <div className="filter">
-          <input type="text" placeholder="Ingresa el término de búsqueda" />
+          <input type="text" placeholder="Ingresa el término de búsqueda" value={filtro} onChange={this.actualizarFiltro} />
         </div>
         <ul>
-          <li>
-            <a href={posts[0].url}><img src={posts[0].image } /></a>
-            <p>{ posts[0].title }</p>
-          </li>
+          {posts.filter(post => post.title.toLowerCase().includes(filtro.toLowerCase())).map((postitem, index) => (
+            <li key={index}>
+              <a alt="no se encontro enlace" href={postitem.url}><img alt={`No se encontro image ${postitem.image}`} src={postitem.image} />
+                <p>{postitem.title}</p>
+              </a>
+            </li>
+          ))}
         </ul>
-      </div>
+
+      </div >
     )
   }
 }
